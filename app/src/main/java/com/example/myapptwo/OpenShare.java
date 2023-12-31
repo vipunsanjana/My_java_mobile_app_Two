12defba0f8bfde5c270dbd5486f9fa1f0b08aec1
vipunsanjana <vipunsanjana34@gmail.com>
2023-12-31 12:14:50 +0530

    webviewandopenshare part devoloped

--- app/src/main/java/com/example/myapptwo/OpenShare.java
+++ app/src/main/java/com/example/myapptwo/OpenShare.java
@@ -2,13 +2,31 @@ package com.example.myapptwo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OpenShare extends AppCompatActivity {
 
+    private Button btn_open_share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_share);
+
+        btn_open_share = findViewById(R.id.btn_share);
+
+        btn_open_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = "Here is the share content body";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+            }
+        });
     }
 }
